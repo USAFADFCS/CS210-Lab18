40862ee4bd46930a4ec93c889528c4ea427f6f94
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-09-25 15:05:22 -0600

    Uploading template

---
+++ lab19functs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "lab19functs.h"
+
+/** lab19functs.c
+* ===========================================================
+* Name: CS210
+* Section:
+* Project: Lab 19
+* Purpose: 
+* ===========================================================
+*/
---
+++ lab19functs.h
@@ -0,0 +1,17 @@
+/** lab19functs.h
+* ===========================================================
+* Name: CS210
+* Section:
+* Project: Lab 19
+* Purpose: 
+* ===========================================================
+*/
+
+void replaceCharInStr(char str[], char searchChar, char replaceChar);
+
+void replaceMultiChar(char str[], char searchChars[], char replaceChar );
+
+int findLocations(char str[], int locations[], char searchChar);
+
+int countSeqStr(char str[], char searchStr[]); 
+
